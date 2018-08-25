63a0807ec7b08b13c0780e26d38f46213c0f7b55
alexander ames <alexanderthomasames@gmail.com>
2018-08-25 16:17:36 -0700

    seed db w make/model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# create 20 makes
+20.times do
+  Make.create(
+    name: Faker::Vehicle.make
+  )
+end
+
+makes = Make.all
+
+# for each make, add 5 models
+makes.each_with_index { |make| 5.times { make.models.create(name: Faker::Vehicle.model(make.name)) } }
