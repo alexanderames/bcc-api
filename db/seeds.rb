33411e2917b22bb4ec948a674c72825589ea6656
alexander ames <alexanderthomasames@gmail.com>
2018-08-26 10:40:55 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,29 @@ end
 makes = Make.all
 
 # for each make, add 5 models
-makes.each_with_index { |make| 5.times { make.models.create(name: Faker::Vehicle.model(make.name)) } }
+makes.each_with_index { |make| make.models.create(name: Faker::Vehicle.model(make.name)) }
+
+# create a vehicle with corresponding make
+makes.each { |make|
+  Vehicle.create(
+    make_id: make.id,
+    vin: Faker::Vehicle.vin,
+    year: Faker::Vehicle.year,
+    plate_state: Faker::Address.state
+  )
+}
+
+models = Model.all
+
+# make options for each model
+models.each do |model|
+  Option.create(
+    model_id: model.id,
+    door_count: Faker::Vehicle.door_count,
+    transmission: Faker::Vehicle.transmission,
+    drivetrain: Faker::Vehicle.drive_type,
+    fuel_type: Faker::Vehicle.fuel_type,
+    style: Faker::Vehicle.style,
+    color: Faker::Vehicle.color
+  )
+end
